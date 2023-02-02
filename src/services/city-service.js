9c85afc472b4c5603ba97aa5b709b6c33add4b5c
ritik <gupta.ritik2002@gmail.com>
2023-02-02 20:33:40 +0530

    added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,50 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+  constructor() {
+    this.cityRepository = new CityRepository(); //binding the object with the cityservice class .
+  }
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      //inside the cityservice class if we want to use the property of the cityservice then we have to use this.property
+      // syntax like we did here ,outside the class if we want to make use of the property of the class
+      //then we have to use like first create a object of the CityServie class then obj.property.
+    } catch (error) {
+      console.log("something went wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId);
+      return response;
+    } catch (error) {
+      console.log("something went wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(data) {
+    try {
+      const city = await this.cityRepository.updateCity(data);
+      return city;
+    } catch (error) {
+      console.log("something went wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId);
+      return city;
+    } catch (error) {
+      console.log("something went wrong at service layer");
+      throw { error };
+    }
+  }
+}
+
+module.exports = CityService;
